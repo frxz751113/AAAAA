d4cc7d021b1d89618c8c25a17e43c976696eeb9d
frxz751113 <156018267+frxz751113@users.noreply.github.com>
2024-08-09 20:59:11 +0800

    Update 分类去重.py

--- IPTV/分类去重.py
+++ IPTV/分类去重.py
@@ -81,7 +81,9 @@ with open('TW.txt', 'w', encoding="utf-8") as file:
  file.writelines(unique_lines)
 
 
-
+for line in fileinput.input("TW.txt", inplace=True):   #打开临时文件原地替换关键字
+    line = line.replace("﻿,ht", "[EXO解码],ht")                         #编辑替换字
+    print(line, end="")   
 
 
 ################################################################################################任务结束，删除不必要的过程文件

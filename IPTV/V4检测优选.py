ce0b01f9e250523b3c4aac5c70c6765dfb2e9586
frxz751113 <156018267+frxz751113@users.noreply.github.com>
2025-08-14 23:46:13 +0800

    Update V4检测优选.py

--- IPTV/V4检测优选.py
+++ IPTV/V4检测优选.py
@@ -16,7 +16,7 @@ import time
 
 
 # 函数：获取视频分辨率
-def get_video_resolution(video_path, timeout=0.8):
+def get_video_resolution(video_path, timeout=3):
     cap = cv2.VideoCapture(video_path)
     if not cap.isOpened():
         return None
